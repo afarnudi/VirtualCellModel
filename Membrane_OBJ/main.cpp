d5b4986b6150934ff2bb53d7b572825f74ecfb9f
ali <a.farnudi@physics.sharif.ir>
2018-06-27 12:17:43 +0430

    Initial Commit

---
+++ Membrane_OBJ/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Membrane_OBJ
+//
+//  Created by Ali Farnudi on 27/06/2018.
+//  Copyright © 2018 Ali Farnudi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
