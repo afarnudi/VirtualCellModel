f85d688efda17850febf91a297e2d35bfd6104c4
ali <a.farnudi@physics.sharif.edu>
2018-11-30 17:04:54 +0330

    forgot to add the thermostat file

---
+++ source/Membrane_Thermostat.cpp
@@ -0,0 +1,59 @@
+#include "Membrane.h"
+
+void Membrane::Thermostat_2(double MD_KT){
+    double V_com[3];
+    
+    V_com[0]=0;
+    V_com[1]=0;
+    V_com[2]=0;
+    
+    for (int i=0; i<Num_of_Nodes; i++) {
+        V_com[0]+=Node_Velocity[i][0];
+        V_com[1]+=Node_Velocity[i][1];
+        V_com[2]+=Node_Velocity[i][2];
+    }
+    
+    
+    V_com[0]/=Num_of_Nodes;
+    V_com[2]/=Num_of_Nodes;
+    V_com[1]/=Num_of_Nodes;
+    
+    //    cout<<"COM before= "<<sqrt(V_com[0]*V_com[0]+V_com[1]*V_com[1]+V_com[2]*V_com[2])<<"\nV_X="<<V_com[0]<<"\tV_Y="<<V_com[1]<<"\tV_Z="<<V_com[2]<<endl;
+    
+    
+    double alpha;
+    //----------------------membrane---------------------
+    
+    //    alpha=sqrt(  (3*Membrane_num_of_Nodes*KT) / kineticenergymembrane( membrane.Node_Velocity )         );/// NOTE THAT THERMOSTATE IS FOR MEMBRANE YET. IN ORDER TO
+    /// UPDATE IT FOR BOTH THE MEMBRANE AND NUCLEI WE HAVE TO
+    /// WRITE  alpha=sqrt(      (2*3*Membrane_num_of_Nodes*KT) / kineticenergy( membrane.Node_Velocity,vnuclei
+    double Kinetic_energy=0;
+    for (int i=0; i<Num_of_Nodes; i++) {
+        Node_Velocity[i][0]-=V_com[0];
+        Node_Velocity[i][1]-=V_com[1];
+        Node_Velocity[i][2]-=V_com[2];
+    }
+    
+    for (int i=0; i<Num_of_Nodes; i++) {
+        Kinetic_energy+=Node_Velocity[i][0]*Node_Velocity[i][0]+Node_Velocity[i][1]*Node_Velocity[i][1]+Node_Velocity[i][2]*Node_Velocity[i][2];
+    }
+    Kinetic_energy*=Node_Mass;
+    
+    
+    
+    alpha=sqrt(3*Num_of_Nodes*MD_KT)/Kinetic_energy;
+//    cout<<"3*Num_of_Nodes*MD_KT="<<3*Num_of_Nodes*MD_KT<<"\tKinetic_energy= "<<Kinetic_energy<<endl;
+//    cout<<"V_com= "<<sqrt(V_com[0]*V_com[0]+V_com[1]*V_com[1]+V_com[2]*V_com[2])<<"\talpha= "<<alpha<<endl;
+    //cout<<alpha<<endl;
+    //cout<<V_com[0]<<"\t"<<V_com[1]<<"\t"<<V_com[2]<<"\n";
+    
+    for(int i=0;i<Num_of_Nodes;i++)
+    {
+        Node_Velocity [i][0]*=alpha;
+        Node_Velocity [i][1]*=alpha;
+        Node_Velocity [i][2]*=alpha;
+        Node_Velocity[i][0]+=V_com[0];
+        Node_Velocity[i][1]+=V_com[1];
+        Node_Velocity[i][2]+=V_com[2];
+    }
+}
