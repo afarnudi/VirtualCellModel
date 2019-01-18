6d3f8e443d61beddec1d0baa1d445cf242e91ed0
Hoda-Shirzad <h.shirzad73@gmail.com>
2019-01-18 21:21:25 +0330

    Merge branch 'master' of https://github.com/afarnudi/Membrane_OBJ

--- source/Chromatin/Chromatin_Thermostat.cpp
+++ source/Chromatin/Chromatin_Thermostat.cpp
@@ -1,55 +1,91 @@
 #include "Chromatin.h"
+#include "General_functions.hpp"
 
 void Chromatin::Thermostat_2(double MD_KT){
-    double V_com[3];
-    
-    V_com[0]=0;
-    V_com[1]=0;
-    V_com[2]=0;
-    
-    for (int i=0; i<Num_of_Nodes; i++) {
-        V_com[0]+=Node_Velocity[i][0];
-        V_com[1]+=Node_Velocity[i][1];
-        V_com[2]+=Node_Velocity[i][2];
-    }
-    
-    
-    V_com[0]/=Num_of_Nodes;
-    V_com[2]/=Num_of_Nodes;
-    V_com[1]/=Num_of_Nodes;
+    update_COM_velocity();
     
     double alpha;
-    
-    //    alpha=sqrt(  (3*Membrane_num_of_Nodes*KT) / kineticenergymembrane( membrane.Node_Velocity )         );/// NOTE THAT THERMOSTATE IS FOR MEMBRANE YET. IN ORDER TO
-    /// UPDATE IT FOR BOTH THE MEMBRANE AND NUCLEI WE HAVE TO
-    /// WRITE  alpha=sqrt(      (2*3*Membrane_num_of_Nodes*KT) / kineticenergy( membrane.Node_Velocity,vnuclei
     double Kinetic_energy=0;
     for (int i=0; i<Num_of_Nodes; i++) {
-        Node_Velocity[i][0]-=V_com[0];
-        Node_Velocity[i][1]-=V_com[1];
-        Node_Velocity[i][2]-=V_com[2];
+        Node_Velocity[i][0]-=COM_velocity[0];
+        Node_Velocity[i][1]-=COM_velocity[1];
+        Node_Velocity[i][2]-=COM_velocity[2];
     }
     
     for (int i=0; i<Num_of_Nodes; i++) {
         Kinetic_energy+=Node_Velocity[i][0]*Node_Velocity[i][0]+Node_Velocity[i][1]*Node_Velocity[i][1]+Node_Velocity[i][2]*Node_Velocity[i][2];
     }
-    Kinetic_energy*=Node_Mass;
+    Kinetic_energy*=Node_Mass/(3*Num_of_Nodes);
     
     
     
-    alpha=sqrt(3*Num_of_Nodes*MD_KT/Kinetic_energy);
-//    cout<<"3*Num_of_Nodes*MD_KT="<<3*Num_of_Nodes*MD_KT<<"\tKinetic_energy= "<<Kinetic_energy<<endl;
-//    cout<<"V_com= "<<sqrt(V_com[0]*V_com[0]+V_com[1]*V_com[1]+V_com[2]*V_com[2])<<"\talpha= "<<alpha<<endl;
-//    cout<<alpha<<endl;
-    //cout<<V_com[0]<<"\t"<<V_com[1]<<"\t"<<V_com[2]<<"\n";
-    
+    alpha=sqrt(MD_KT/Kinetic_energy);
     for(int i=0;i<Num_of_Nodes;i++)
     {
         Node_Velocity [i][0]*=alpha;
         Node_Velocity [i][1]*=alpha;
         Node_Velocity [i][2]*=alpha;
-        Node_Velocity[i][0]+=V_com[0];
-        Node_Velocity[i][1]+=V_com[1];
-        Node_Velocity[i][2]+=V_com[2];
+        Node_Velocity[i][0]+=COM_velocity[0];
+        Node_Velocity[i][1]+=COM_velocity[1];
+        Node_Velocity[i][2]+=COM_velocity[2];
+    }
+}
+
+void Chromatin::Thermostat_N6(double MD_KT){
+    
+    update_COM_velocity();
+    update_COM_position();
+    double COM_omega[3]={0};
+    double temp_cross[3]={0};
+    
+    for (int i=0; i<Num_of_Nodes; i++) {
+        Node_Velocity[i][0] -= COM_velocity[0];
+        Node_Velocity[i][1] -= COM_velocity[1];
+        Node_Velocity[i][2] -= COM_velocity[2];
+        
+        Node_Position[i][0] -= COM_position[0];
+        Node_Position[i][1] -= COM_position[1];
+        Node_Position[i][2] -= COM_position[2];
+        
+        double a[3]={Node_Position[i][0], Node_Position[i][1], Node_Position[i][2]};
+        double b[3]={Node_Velocity[i][0], Node_Velocity[i][1], Node_Velocity[i][2]};
+        crossvector(temp_cross, a, b);
+        double len=vector_length_squared(temp_cross);
+        COM_omega[0]+=temp_cross[0]/len;
+        COM_omega[1]+=temp_cross[1]/len;
+        COM_omega[2]+=temp_cross[2]/len;
+    }
+    
+    double Kinetic_energy=0;
+    for (int i=0; i<Num_of_Nodes; i++) {
+        double a[3]={Node_Position[i][0], Node_Position[i][1], Node_Position[i][2]};
+        crossvector(temp_cross, COM_omega, a);
+        Node_Velocity[i][0]-=temp_cross[0];
+        Node_Velocity[i][1]-=temp_cross[1];
+        Node_Velocity[i][2]-=temp_cross[2];
+        double b[3]={Node_Velocity[i][0], Node_Velocity[i][1], Node_Velocity[i][2]};
+        Kinetic_energy+=vector_length_squared(b);
+    }
+    double initial_temperature=Kinetic_energy*Node_Mass/(3*Num_of_Nodes-6);
+    
+    double alpha=sqrt(MD_KT/initial_temperature);
+    for(int i=0;i<Num_of_Nodes;i++)
+    {
+        Node_Velocity [i][0]*=alpha;
+        Node_Velocity [i][1]*=alpha;
+        Node_Velocity [i][2]*=alpha;
+        
+        double a[3]={Node_Position[i][0], Node_Position[i][1], Node_Position[i][2]};
+        crossvector(temp_cross, COM_omega, a);
+        
+        Node_Velocity[i][0]+=COM_velocity[0]+temp_cross[0];
+        Node_Velocity[i][1]+=COM_velocity[1]+temp_cross[1];
+        Node_Velocity[i][2]+=COM_velocity[2]+temp_cross[2];
+        
+        Node_Position[i][0]+=COM_position[0];
+        Node_Position[i][1]+=COM_position[1];
+        Node_Position[i][2]+=COM_position[2];
+        
+        
     }
 }
