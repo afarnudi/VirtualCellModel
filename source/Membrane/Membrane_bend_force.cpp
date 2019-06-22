bab742917d6e60a59fdfea0978a75bd4f24c9791
ali <a.farnudi@physics.sharif.edu>
2019-06-22 13:04:40 +0430

    reverted the bending force sign calculation to the original form. The new version had a nasty bug.

--- source/Membrane/Membrane_bend_force.cpp
+++ source/Membrane/Membrane_bend_force.cpp
@@ -35,16 +35,10 @@ void Membrane::Bending_potetial_2(double sin_theta_0){
         double N1dotN2=innerproduct(N1, N2)/(N1_length*N2_length);
         sin=(1.000001-N1dotN2)/2;
         if(sin<0){
-        sin=-sin;
+            sin=-sin;
         }
         sin=sqrt(sin);
-        double a=innerproduct(N3, E);
-        double b =a*a;
-        b=sqrt(b);
-        double s=a/b;
-       sign=-(1-2*s);
-       
-       //sign=-(1-2*signbit(innerproduct(N3, E)));
+        sign=-(1-2*std::signbit(innerproduct(N3, E)));
         F0 = (sin-sin_theta_0)*sign*E_length*E_length*Bending_coefficient*sin/(N1_length+N2_length);
         
         double temp_1, temp_2, temp_3_1, temp_3_2, temp_4_1, temp_4_2;
