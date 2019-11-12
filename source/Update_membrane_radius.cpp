47bbed781620cdfe8c0edbed9c24f2105569afa7
ali <a.farnudi@physics.sharif.edu>
2019-11-12 09:55:39 +0330

    Forgot to resolve a minor merge conflict

--- source/Update_membrane_radius.cpp
+++ source/Update_membrane_radius.cpp
@@ -31,34 +31,6 @@ bool check_for_membrane_update(vector<Membrane>    &membranes,
 }
 
 
-double updated_sigam_value(Membrane &mem, double time){
-    double t1, t2, r, rnew, dt, a, b;
-    // All calculations in Fs
-    t1   = mem.get_Begin_update_time_in_Ps()*1000;
-    t2   = mem.get_End_update_time_in_Ps()*1000;
-    r    = mem.get_node_radius();
-    rnew = mem.get_new_node_radius();
-    dt   = t2-t1;
-    // sigma_ev = a * time + b
-    a    = (rnew - r)/dt;
-    b    = (r*t2 - rnew* t1)/dt;
-    
-    return  a * time * 1000 + b;
-}
-double updated_length_value(Membrane &mem, double time){
-    double t1, t2, r, rnew, dt, a, b;
-    // All calculations in Fs
-    t1   = mem.get_Begin_update_time_in_Ps()*1000;
-    t2   = mem.get_End_update_time_in_Ps()*1000;
-    r    = mem.get_node_radius();
-    rnew = mem.get_new_node_radius();
-    dt   = t2-t1;
-    // sigma_ev = a * time + b
-    a    = (rnew - r)/dt;
-    b    = (r*t2 - rnew* t1)/dt;
-    
-    return  a * time * 1000 + b;
-}
 /** -----------------------------------------------------------------------------
  */
 void updateOpenMMforces(vector<Membrane>                &membranes,
@@ -72,15 +44,10 @@ void updateOpenMMforces(vector<Membrane>                &membranes,
     
     int mem_count=0;
     
-<<<<<<< HEAD
-//    double t1, t2, r, rnew, dt, a, b;
-=======
     double t1, t2, r, rnew, dt, a, b;
     double new_radius=0;
->>>>>>> c869f3273c2b5c5cf57b5ce4cb75ac82185f29f5
     
     for (int i=0; i<membranes.size(); i++) {
-        //Update node radius
         if (membranes[i].get_new_node_radius() != -1) {
             if (time > membranes[i].get_Begin_update_time_in_Ps() &&
                 time < membranes[i].get_End_update_time_in_Ps()         )
@@ -96,107 +63,6 @@ void updateOpenMMforces(vector<Membrane>                &membranes,
                 b    = (r*t2 - rnew* t1)/dt;
                 
                 
-<<<<<<< HEAD
-                switch (interaction_map[i][j]) {
-                        
-                    case 1:
-                        
-                        break;
-                        
-                    case 2:
-                        
-                        set<int> :: iterator it_1 = membrane_set[i].begin();
-                        set<int> :: iterator it_2 = membrane_set[j].begin();
-                        
-                        omm->EV[EV_index]-> setCutoffDistance( 1.5 * ( atoms[*it_1].radius
-                                                                      + atoms[*it_2].radius )
-                                                              * OpenMM::NmPerAngstrom
-                                                              );
-                        
-                        EV_index++;
-                        break;
-                        
-                }
-            }
-        }
-        
-        
-        int class_count_i, class_count_j;
-        
-        for (int i=0; i < GenConst::Num_of_Actins; i++) {
-            
-            vector<double> sigma_ev(1, updated_sigam_value(membranes[i], time));
-            
-            for (int node_index = mem_count; node_index < membranes[i].get_num_of_nodes() + mem_count; node_index++) {
-                
-                atoms[node_index].radius = sigma_ev[0];
-                
-                sigma_ev[0] *= OpenMM::NmPerAngstrom;
-                
-                for (int j=0; j<omm->EV.size(); j++) {
-                    
-                    omm->EV[j]->setParticleParameters(node_index, sigma_ev);
-                }
-            }
-        }
-        vector<double> params(1,0);
-        omm->EV[1]->getParticleParameters(0, params);
-        cout<<"param = "<<params[0]<<endl;
-        
-        //Update spring nominal length
-//        if (membranes[i].get_new_nominal_length() != -1) {
-//
-//            vector<double> sigma_ev(1, updated_sigam_value(membranes[i], time));
-//
-//            for (int node_index = mem_count; node_index < membranes[i].get_num_of_nodes() + mem_count; node_index++) {
-//
-//                atoms[node_index].radius = sigma_ev[0];
-//                sigma_ev[0] *= OpenMM::NmPerAngstrom;
-//
-//                for (int j=0; j<omm->EV.size(); j++) {
-//                    omm->EV[j]->setParticleParameters(node_index, sigma_ev);
-//                }
-//            }
-//        }
-        
-        mem_count += membranes[i].get_num_of_nodes();
-        for (int i=0; i < GenConst::Num_of_Chromatins; i++) {
-            
-            for (int j=0; j < GenConst::Num_of_Membranes; j++) {
-                
-                switch (interaction_map[i + class_count_i][j]) {
-                    case 1:
-                        
-                        
-                        break;
-                    case 2:
-                        set<int> :: iterator it_1 = chromatin_set[i][0].begin();
-                        set<int> :: iterator it_2 = membrane_set[j].begin();
-//                        set<int> :: iterator it_1 = chromatin_set[i].begin();
-//                        set<int> :: iterator it_2 = membrane_set[j].begin();
-                        
-                        omm->EV[EV_index]-> setCutoffDistance( 1.5 * ( atoms[*it_1].radius
-                                                                      + atoms[*it_2].radius )
-                                                              * OpenMM::NmPerAngstrom
-                                                              );
-                        
-                        EV_index++;
-                        break;
-                }
-                
-            }
-            
-            
-        }
-    }
-    
-    cout<<"cutoff before = "<<omm->EV[0]->getCutoffDistance() <<endl;
-    for (int i=0; i< omm->EV.size(); i++) {
-        omm->EV[i]->updateParametersInContext(*omm->context);
-    }
-    cout<<"cutoff after = "<<omm->EV[0]->getCutoffDistance() <<endl;
-//    exit(EXIT_SUCCESS);
-=======
                 string sigma = "sigma" + GenConst::Membrane_label + std::to_string(i) + GenConst::Membrane_label + std::to_string(i) ;
                 double new_sig = (a * time * 1000 + b);
                 new_radius = new_sig;
@@ -214,7 +80,6 @@ void updateOpenMMforces(vector<Membrane>                &membranes,
         mem_count += membranes[i].get_num_of_nodes();
     }
 //    cout<<"2*new_radius = "<<2*new_radius<<endl;
->>>>>>> c869f3273c2b5c5cf57b5ce4cb75ac82185f29f5
     int mem_bond_count=0;
     for (int i=0; i<membranes.size(); i++) {
         if (membranes[i].get_new_node_radius() != -1) {
