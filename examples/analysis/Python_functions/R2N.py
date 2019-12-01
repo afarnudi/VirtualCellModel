Python analysis corrected the units

examples/analysis/Python_functions/R2N.py
@@ -14,7 +14,7 @@ def R2N(data, lab, ax):
         r2n=np.append(r2n,np.mean(np.sqrt(np.mean(np.sum(np.square(data[:,:-j,:]-data[:,j:,:]),axis=2),axis=1))))
         
     ax.scatter(range(1,N+1,10),r2n, label=lab)
-    ax.set_title("R2 of bps along the chain of structures (averaged over time)")
+    ax.set_title("R^2 of bps separated along the chain\n(averaged over time)")
     ax.set_xlabel("N_bb")
     ax.set_ylabel("R2(N_bb)")
     ax.grid(True, which='both')

examples/analysis/zCM.py
@@ -4,14 +4,16 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+from __future__ import print_function
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 filename = 'chromo2019_11_18_time_23_15'
 filename = 'chromo2019_11_22_time_11_40'
 
 filename = 'chromo2019_11_23_time_07_29'
+filename = str(sys.argv[1]) 
 
 velname = filename + '_vels_forces.txt'
 pdbname = filename + '.pdb'
@@ -31,8 +33,9 @@ import matplotlib.animation
 fig, ax = plt.subplots()
 
 
-cutoff = 2.5
-init_t = 500
+cutoff = 4
+
+init_t = 250
 fin_t  = 2000
 
 eye = np.eye(dim1)
@@ -45,8 +48,10 @@ label = "\ntime: "+str(init_t)+" to "+str(fs)+"  cut off: "+str(cutoff)+" Ang"
 ax.set_title(title)
 ax.set_xlabel(label)
 #im = ax.matshow(d , cmap=plt.cm.Blues)
-im = ax.matshow(d , cmap=plt.cm.RdBu)
+im = ax.matshow(d , cmap=plt.cm.jet)
 plt.colorbar(im);
+
+time_bin=10
 
 def update(i):
     global d
@@ -56,16 +61,22 @@ def update(i):
         d = d.astype(float)
         return d
     else :
-        b = squareform( pdist(data[init_t+i, :, :]) ) < cutoff
-        b = b.astype(int)
-        d += b - eye
+        c = np.zeros((dim1,dim1))
+        for j in range(1,time_bin+1):
+            b = squareform( pdist(data[init_t+(i-1)*time_bin+j, :, :]) ) < cutoff
+            c += b.astype(int)
+        c = c.astype(bool)
+        c = c.astype(int)
+        d += c - eye
         return d/np.amax(d)
 
 def animate(i):
     im.set_data(update(i))
+    print(i*time_bin+init_t, end='\r')
+    sys.stdout.flush()
     return
     
 
-ani = matplotlib.animation.FuncAnimation(fig, animate, frames = fin_t-init_t, interval=2)
+ani = matplotlib.animation.FuncAnimation(fig, animate, frames = (fin_t-init_t)//time_bin, interval=1)
 ani.save(filename+'_cm.gif', writer='imagemagick', fps=fs//10)
 

examples/analysis/zcalc_pressure.py
@@ -36,8 +36,9 @@ def calc_pressure(data, Vels, mnodes=162):
     vs = Vels[:,:,:3]
     fs = Vels[:,:,3:-1]
 
-    unit1=24 # 1Kcal = 24 gr.ang^2/Ps^2.mol
-    p=np.sum(np.sum(np.square(vs),axis=2)*mass, axis=1) + unit1*np.sum(np.sum(data*fs, axis=2), axis=1)
+    unit1=4.182 
+    unit2=0.174 # 1Kcal = 24 gr.ang^2/Ps^2.mol
+    p=unit2*np.sum(np.sum(np.square(vs),axis=2)*mass, axis=1) + unit1*np.sum(np.sum(data*fs, axis=2), axis=1)
     return p/3
 
 
@@ -61,7 +62,8 @@ memrg3= 4*np.pi*memrg*memrg*memrg/3
 
 
 axes[0,0].plot(time,memrg, label='mem', color='orange')
-axes[0,0].set_title('Radius of Gyration')
+axes[0,0].set_title("Membrane Radius")
+axes[0,0].set_ylabel("R_g in Angstrom")
 
 p = calc_pressure(traj_data, vel_data)
 p=p/memrg3
@@ -71,8 +73,7 @@ pc = pc/memrg3
 
 axes[0,1].plot(time,p, label='mem + chromos', color='green')
 axes[0,1].set_title('Pressure')
-#axes[0,1].plot(time,pc, label='chromo Pressure', color='blue')
-# =============================================================================
+axes[0,1].set_ylabel('P (1.6*GPa)')
 
 ch0rg = gyration(traj_data[:,mem_nodes:1000 + mem_nodes,:], time)
 ch1rg = gyration(traj_data[:,1000 + mem_nodes:2000 + mem_nodes,:], time)
@@ -84,11 +85,12 @@ axes[1,0].plot(time,ch1rg, label='ch1')
 axes[1,0].plot(time,ch2rg, label='ch2')
 axes[1,0].plot(time,ch3rg, label='ch3')
 axes[1,0].set_title("Radius of Gyration")
+axes[1,0].set_ylabel("R_g in Angstrom")
 
 ch_rdius = 1
-mem_radius = 7
+mem_radius = 4
 ch_volume = ch_rdius*ch_rdius*ch_rdius*4000
-eff_radius = memrg - 7
+eff_radius = memrg - mem_radius
 Mem_volume = eff_radius*eff_radius*eff_radius
 volume_fraction = ch_volume/Mem_volume
 