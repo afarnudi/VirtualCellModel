ad6949de2ccb0aa46ea45fbb1b6f2b7b868e4a43
ali <a.farnudi@physics.sharif.edu>
2020-02-12 18:30:38 +0100

    New implamentation: in case a multiple devices are associated with the same platform, the user is given choice between the devices.

--- source/OpenMM/init_openmm.cpp
+++ source/OpenMM/init_openmm.cpp
@@ -34,26 +34,26 @@ MyOpenMMData* myInitializeOpenMM(const MyAtomInfo       atoms[],
                                  vector<set<int> >      &actin_set,
                                  vector<set<int> >      &ecm_set,
                                  vector<vector<set<int> >  >    &chromatin_set,
-//                                 vector<set<int> >      &chromatin_set,
+                                 //                                 vector<set<int> >      &chromatin_set,
                                  vector<vector<int> >   interaction_map)
 {
-	const string cbp_plugin_location="/scratch/alifarnudi/local/openmm/lib/plugins";
+    const string cbp_plugin_location="/scratch/alifarnudi/local/openmm/lib/plugins";
     // Load all available OpenMM plugins from their default location.
     OpenMM::Platform::loadPluginsFromDirectory(OpenMM::Platform::getDefaultPluginsDirectory());
     //OpenMM::Platform::loadPluginsFromDirectory(cbp_plugin_location);
-
+    
     // Allocate space to hold OpenMM objects while we're using them.
     MyOpenMMData*       omm = new MyOpenMMData();
-
+    
     // Create a System and Force objects within the System.
     OpenMM::System&     system = *(omm->system = new OpenMM::System());
-
+    
     // Retain a reference to each force object so we can fill in the forces.
     // Note: the System owns the force objects and will take care of deleting them;
     // don't do it yourself!
-
+    
     cout<<"Defining interactions...\n";
-
+    
     
     // Create a vector of handles for the force objects. These handles will be added to the system. Each handle in the list will be associated with a class instance.
     vector<OpenMM::CustomNonbondedForce*> ExcludedVolumes;
@@ -63,7 +63,7 @@ MyOpenMMData* myInitializeOpenMM(const MyAtomInfo       atoms[],
     
     OpenMM::CMMotionRemover* comremover;
     comremover = new OpenMM::CMMotionRemover(GenConst::CMMotionRemoverStep);
-
+    
     set_interactions(atoms,
                      bonds,
                      membrane_set,
@@ -113,8 +113,8 @@ MyOpenMMData* myInitializeOpenMM(const MyAtomInfo       atoms[],
     
     // Add the list of atom pairs that are excluded from the excluded volume force.
     // the second input is an integer, bondCutoff; OpenMM defines bondCutoff as "pairs of particles that are separated by this many bonds or fewer are added to the list of exclusions".
-
-
+    
+    
     
     
     vector<OpenMM::CustomCompoundBondForce*> DihedralForces;
@@ -125,8 +125,8 @@ MyOpenMMData* myInitializeOpenMM(const MyAtomInfo       atoms[],
     if (dihedrals[0].type != EndOfList) {
         omm->Dihedral = DihedralForces;
     }
-
-    cout<<"platform default directory path = "<<OpenMM::Platform::getDefaultPluginsDirectory()<<endl;
+    
+    //cout<<"platform default directory path = "<<OpenMM::Platform::getDefaultPluginsDirectory()<<endl;
     //Listing the names of all available platforms.
     cout<<"OpenMM available platforms:\nPlatform name  Estimated speed\n";
     for (int i = 0; i < OpenMM::Platform::getNumPlatforms(); i++) {
@@ -138,6 +138,87 @@ MyOpenMMData* myInitializeOpenMM(const MyAtomInfo       atoms[],
     std::cin>>platform_id;
     OpenMM::Platform& platform = OpenMM::Platform::getPlatform(platform_id);
     
+    
+    std::vector<std::map<std::string, std::string> > device_properties;
+    if (platform.getName() == "OpenCL") {
+        cout<<"Available devices on the "<<platform.getName()<<" platform:\n";
+        int counter=0;
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                try {
+                    std::map<std::string, std::string> temp_device_properties;
+                    temp_device_properties["OpenCLPlatformIndex"]=std::to_string(i);
+                    temp_device_properties["OpenCLDeviceIndex"]=std::to_string(j);
+                    OpenMM::System temp_system;
+                    temp_system.addParticle(1.0);
+                    OpenMM::VerletIntegrator temp_inegrator(stepSizeInFs * OpenMM::PsPerFs);
+                    OpenMM::Context temp_context(temp_system, temp_inegrator, platform, temp_device_properties);
+                    std::vector<std::string> platform_devices = platform.getPropertyNames();
+                    cout<<counter<<" : \t";
+                    for (auto & name : platform_devices){
+                        if (name == "DeviceName") {
+                            cout<<platform.getPropertyValue(temp_context, name)<<endl;
+                        }
+                        if (name == "Precision") {
+                            cout<<"\t"<<name<<" = "<<platform.getPropertyValue(temp_context, name)<<endl;
+                        }
+                    }
+                    cout<<endl;
+                    device_properties.push_back(temp_device_properties);
+                } catch (const std::exception& e) {
+                    
+                }
+            }
+        }
+    } else if (platform.getName() == "CUDA") {
+        cout<<"Available devices on the "<<platform.getName()<<" platform:\n";
+        int counter=0;
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                try {
+                    std::map<std::string, std::string> temp_device_properties;
+                    temp_device_properties["CudaPlatformIndex"]=std::to_string(i);
+                    temp_device_properties["CudaDeviceIndex"]=std::to_string(j);
+                    OpenMM::System temp_system;
+                    temp_system.addParticle(1.0);
+                    OpenMM::VerletIntegrator temp_inegrator(stepSizeInFs * OpenMM::PsPerFs);
+                    OpenMM::Context temp_context(temp_system, temp_inegrator, platform, temp_device_properties);
+                    std::vector<std::string> platform_devices = platform.getPropertyNames();
+                    cout<<counter<<" : \t";
+                    for (auto & name : platform_devices){
+                        if (name == "DeviceName") {
+                            cout<<platform.getPropertyValue(temp_context, name)<<endl;
+                        }
+                        if (name == "Precision") {
+                            cout<<"\t"<<name<<" = "<<platform.getPropertyValue(temp_context, name)<<endl;
+                        }
+                    }
+                    cout<<endl;
+                    device_properties.push_back(temp_device_properties);
+                } catch (const std::exception& e) {
+                    
+                }
+            }
+        }
+    } else if (platform.getName() == "CPU") {
+        OpenMM::System temp_system;
+        temp_system.addParticle(1.0);
+        OpenMM::VerletIntegrator temp_inegrator(stepSizeInFs * OpenMM::PsPerFs);
+        OpenMM::Context temp_context(temp_system, temp_inegrator, platform);
+        std::vector<std::string> platform_devices = platform.getPropertyNames();
+        for (auto & name : platform_devices){
+            if (name == "Threads") {
+                cout<<"CPU threads : "<<platform.getPropertyValue(temp_context, name)<<endl;
+            }
+        }
+        cout<<endl;
+    }
+    
+    int device_id=0;
+    if (device_properties.size()>1) {
+        cout<<"Please choose a device (index): \n";
+        std::cin>>device_id;
+    }
     
     // Choose an Integrator for advancing time, and a Context connecting the
     // System with the Integrator for simulation. Let the Context choose the
@@ -165,19 +246,15 @@ MyOpenMMData* myInitializeOpenMM(const MyAtomInfo       atoms[],
     if (GenConst::CMMotionRemover) {
         omm->system->addForce(comremover);
     }
-    
-    //omm->context    = new OpenMM::Context(*omm->system, *omm->integrator, platform);
-    const OpenMM::Context lcontext(*omm->system, *omm->integrator, platform);
-    //omm->context->setPositions(initialPosInNm);
-    //omm->context->setVelocities(initialVelInNmperPs);
-    //platformName = omm->context->getPlatform().getName();
-    
-    std::vector<std::string> platform_devices = platform.getPropertyNames();
-    for (auto & name : platform_devices){
-        cout<<name<<"\t"<<platform.getPropertyValue(lcontext, name)<<endl;
-        
+    if (platform.getName() == "CPU" || platform_id==0) {
+        omm->context    = new OpenMM::Context(*omm->system, *omm->integrator, platform);
+    } else {
+        omm->context    = new OpenMM::Context(*omm->system, *omm->integrator, platform, device_properties[device_id]);
     }
-    exit(EXIT_SUCCESS);
-
+    
+    omm->context->setPositions(initialPosInNm);
+    omm->context->setVelocities(initialVelInNmperPs);
+    platformName = omm->context->getPlatform().getName();
+    
     return omm;
 }
