98fdcbd9420dde3e680f1a6612aca251912e9b34
ali <a.farnudi@physics.sharif.edu>
2020-10-07 13:15:07 +0200

    Forgot to add these new files to the previous commit. One creates directories tree for runs and the other sets the chromatin chain nominal lengths.

---
+++ source/Chromatin/Chromatin_set_nominl_lengths.cpp
@@ -0,0 +1,60 @@
+//
+//  Initialise.cpp
+//  Membrae
+//
+//  Created by Ali Farnudi on 14/10/2018.
+//  Copyright © 2018 Ali Farnudi. All rights reserved.
+//
+
+#include "Chromatin.h"
+#include <algorithm>
+//void build_random_chain(void);
+
+using namespace std;
+
+void Chromatin::set_bond_nominal_lengths(void){
+    BondNominalLengths.clear();
+    BondNominalLengths.resize(Num_of_Nodes-1,0);
+    
+    Min_node_pair_length=100000000000000000000.;
+    Max_node_pair_length=0;
+    Average_node_pair_length=0;
+    
+    
+    for (int i=1; i<Num_of_Nodes; i++) {
+        double bondvec[3]={Node_Position[i][0]-Node_Position[i-1][0],
+                           Node_Position[i][1]-Node_Position[i-1][1],
+                           Node_Position[i][2]-Node_Position[i-1][2]};
+        BondNominalLengths[i-1]=vector_length(bondvec);
+        
+        Average_node_pair_length += BondNominalLengths[i-1];
+        
+        if (BondNominalLengths[i-1] < Min_node_pair_length) {
+            Min_node_pair_length = BondNominalLengths[i-1];
+        }
+        if (BondNominalLengths[i-1] > Max_node_pair_length) {
+            Max_node_pair_length = BondNominalLengths[i-1];
+        }
+        
+    }
+    
+    if (BondNominalLength_stat=="Au") {
+        if (!GenConst::Testmode) {
+            cout<<"Node pair (bond) distances:\n";
+            cout<<"\tMax "<<Max_node_pair_length<<"\tMin "<<Min_node_pair_length<<"\tAverage "<<Average_node_pair_length<<endl;
+        }
+    } else if (BondNominalLength_stat=="Av"){
+        BondNominalLengths.clear();
+        BondNominalLengths.resize(Num_of_Nodes-1,Average_node_pair_length);
+        if (!GenConst::Testmode) {
+            cout<<"Average node pair (bond) distance "<<TFILE<<Average_node_pair_length<<TRESET<<" will be set for the spring nominal length."<<endl;
+        }
+    } else {
+        BondNominalLengths.clear();
+        BondNominalLengths.resize(Num_of_Nodes,stod(BondNominalLength_stat));
+        if (!GenConst::Testmode) {
+            cout<<"User defined nominal length ("<<TFILE<<BondNominalLength_stat<<TRESET<<") will be used."<<endl;
+        }
+    }
+    
+}
---
+++ source/Genfuncs/AssignProjectDirectory.cpp
@@ -0,0 +1,57 @@
+//
+//  General_functions.cpp
+//  Cell-Durotaxis
+//
+//  Created by Ali Farnudi on 27/08/2017.
+//  Copyright © 2017 Ali Farnudi. All rights reserved.
+//
+#include <iostream>
+#include <fstream>
+
+#include "General_constants.h"
+#include "Arg_pars.hpp"
+#include "cxxopts.hpp"
+#include "Configfile.hpp"
+#include "Membrane.h"
+#include "Actin.h"
+#include "ECM.h"
+#include "Chromatin.h"
+
+using namespace std;
+
+
+#include <boost/filesystem.hpp>
+
+void assign_project_directories(char* buffer){
+    int instanceID=0;
+    string trajectory = "Results";
+    boost::filesystem::path pTrajectory(trajectory);
+    if(!boost::filesystem::exists(pTrajectory)){
+        boost::filesystem::create_directory(pTrajectory);
+    }
+    trajectory +="/"+GenConst::ProjectName;
+    pTrajectory=trajectory;
+    if(!boost::filesystem::exists(pTrajectory)){
+        boost::filesystem::create_directory(pTrajectory);
+    }
+    trajectory += "/";
+    trajectory += buffer;
+    
+    string IDtoString = to_string(instanceID);
+    string ID = string(3 - IDtoString.length(), '0') + IDtoString;
+    pTrajectory=trajectory+"_"+ID;
+    while(boost::filesystem::exists(pTrajectory)){
+        instanceID++;
+        IDtoString = to_string(instanceID);
+        ID = string(3 - IDtoString.length(), '0') + IDtoString;
+        pTrajectory=trajectory+"_"+ID;
+    }
+    boost::filesystem::create_directory(pTrajectory);
+    
+    trajectory+="_"+ID;
+    trajectory+="/";
+    trajectory+= buffer;
+    trajectory+="_"+ID;
+    cout<<trajectory<<endl;
+    GenConst::trajectory_file_name=trajectory;
+}
