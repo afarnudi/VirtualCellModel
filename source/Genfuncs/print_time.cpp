a8f72d69d0e830de159700ffd3db721884e3717c
ali <a.farnudi@physics.sharif.edu>
2020-10-12 14:55:38 +0200

    Added hardware report. It prints in a file in the same directory as the trajectories.

--- source/Genfuncs/print_time.cpp
+++ source/Genfuncs/print_time.cpp
@@ -9,6 +9,10 @@
 #include "General_functions.hpp"
 
 void print_wall_clock_time(double sim_duration_per_sec){
+    std::string traj_name  = GenConst::trajectory_file_name+"_hardware_runtime.txt";
+    FILE* pFile;
+    pFile = fopen (traj_name.c_str(),"a");
+    
     
     double sec_per_day     =60*60*24;
     double sec_per_hour    =60*60;
@@ -25,8 +29,11 @@ void print_wall_clock_time(double sim_duration_per_sec){
     sim_duration_per_sec -= mins * sec_per_min;
     
     printf("Wall clock time of the simulation:\n");
-    //            printf("%.2f Days,\n%.2f Hours,\n%.2f Minutes,\n%.2f Seconds\n", days,hours,mins,sim_duration_per_sec);
     printf("%4i\tHours,\n%4i\tMinutes,\n%4i\tSeconds\n", hours,mins,int(sim_duration_per_sec) );
+    
+    fprintf(pFile,"Wall clock time of the simulation:\n");
+    fprintf(pFile,"%4i\tHours,\n%4i\tMinutes,\n%4i\tSeconds\n", hours,mins,int(sim_duration_per_sec) );
+    fclose (pFile);
 }
 
 //using std::chrono;
@@ -34,32 +41,46 @@ using std::cout;
 
 void print_real_time(std::chrono::time_point<std::chrono::steady_clock> chrono_clock_start,
                      std::chrono::time_point<std::chrono::steady_clock> chrono_clock_end){
+    std::string hardname= GenConst::trajectory_file_name+"_hardware_runtime.txt";
+    std::ofstream write_report;
+    write_report.open(hardname.c_str(),std::ios_base::app);
     
     auto chromo_clock_diff = chrono_clock_end - chrono_clock_start;
     int secs;
     int hours;
     int mins;
     cout << "\nchrono::steady_clock runtime: \n";
+    write_report << "\nchrono::steady_clock runtime: \n";
     hours = std::chrono::duration_cast<std::chrono::hours>(chromo_clock_diff).count();
     cout << hours << "\tHours\n";
+    write_report << hours << "\tHours\n";
     mins  = std::chrono::duration_cast<std::chrono::minutes>(chromo_clock_diff).count();
     cout << mins - hours * 60 << "\tMinutes\n";
+    write_report << mins - hours * 60 << "\tMinutes\n";
     secs  = std::chrono::duration_cast<std::chrono::seconds>(chromo_clock_diff).count();
     cout << secs - mins * 60 << "\tSeconds\n";
+    write_report << secs - mins * 60 << "\tSeconds\n";
 }
 
 void print_system_time(std::chrono::time_point<std::chrono::system_clock> chrono_clock_start,
                        std::chrono::time_point<std::chrono::system_clock> chrono_clock_end){
+    std::string hardname= GenConst::trajectory_file_name+"_hardware_runtime.txt";
+    std::ofstream write_report;
+    write_report.open(hardname.c_str(),std::ios_base::app);
     
     auto chromo_clock_diff = chrono_clock_end - chrono_clock_start;
     int secs;
     int hours;
     int mins;
     cout << "\nchrono::system_clock runtime: \n";
+    write_report << "\nchrono::system_clock runtime: \n";
     hours = std::chrono::duration_cast<std::chrono::hours>(chromo_clock_diff).count();
     cout << hours << "\tHours\n";
+    write_report << hours << "\tHours\n";
     mins  = std::chrono::duration_cast<std::chrono::minutes>(chromo_clock_diff).count();
     cout << mins - hours * 60 << "\tMinutes\n";
+    write_report << mins - hours * 60 << "\tMinutes\n";
     secs  = std::chrono::duration_cast<std::chrono::seconds>(chromo_clock_diff).count();
     cout << secs - mins * 60 << "\tSeconds\n";
+    write_report << secs - mins * 60 << "\tSeconds\n";
 }
