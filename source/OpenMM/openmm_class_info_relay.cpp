9c16953fb90887835977013b722123c7c55d1999
ali <a.farnudi@physics.sharif.edu>
2021-03-22 16:49:53 +0100

    BugFix: Memebrane correctly initiates if the bond and dihedral interaction is set to None

--- source/OpenMM/openmm_class_info_relay.cpp
+++ source/OpenMM/openmm_class_info_relay.cpp
@@ -37,22 +37,27 @@ void OpenMM_membrane_info_relay (vector<Membrane>       membranes,
         cout<<TMEM<<"Membrane "<<i<<TRESET<<endl;
         cout<<"Bond potential:";
         Bonds* bonds = convert_membrane_bond_info_to_openmm(membranes[i]);
-        for (int j=0; j<membranes[i].get_num_of_node_pairs(); j++) {
-            all_bonds[j+bond_count]=bonds[j];
-            all_bonds[j+bond_count].atoms[0]=bonds[j].atoms[0]+atom_count;
-            all_bonds[j+bond_count].atoms[1]=bonds[j].atoms[1]+atom_count;
-            
+        if (membranes[i].get_spring_model() != potentialModelIndex.Model["None"]) {
+            for (int j=0; j<membranes[i].get_num_of_node_pairs(); j++) {
+                all_bonds[j+bond_count]=bonds[j];
+                all_bonds[j+bond_count].atoms[0]=bonds[j].atoms[0]+atom_count;
+                all_bonds[j+bond_count].atoms[1]=bonds[j].atoms[1]+atom_count;
+                
+            }
         }
         
         cout<<"Dihedral potential:";
         Dihedrals* dihedrals = convert_membrane_dihedral_info_to_openmm(membranes[i]);
-        for (int j=0; j<membranes[i].get_num_of_triangle_pairs(); j++) {
-            all_dihedrals[j+dihe_count]=dihedrals[j];
-            all_dihedrals[j+dihe_count].atoms[0]=dihedrals[j].atoms[0]+atom_count;
-            all_dihedrals[j+dihe_count].atoms[1]=dihedrals[j].atoms[1]+atom_count;
-            all_dihedrals[j+dihe_count].atoms[2]=dihedrals[j].atoms[2]+atom_count;
-            all_dihedrals[j+dihe_count].atoms[3]=dihedrals[j].atoms[3]+atom_count;
+        if (membranes[i].get_bending_model() != potentialModelIndex.Model["None"]) {
+            for (int j=0; j<membranes[i].get_num_of_triangle_pairs(); j++) {
+                all_dihedrals[j+dihe_count]=dihedrals[j];
+                all_dihedrals[j+dihe_count].atoms[0]=dihedrals[j].atoms[0]+atom_count;
+                all_dihedrals[j+dihe_count].atoms[1]=dihedrals[j].atoms[1]+atom_count;
+                all_dihedrals[j+dihe_count].atoms[2]=dihedrals[j].atoms[2]+atom_count;
+                all_dihedrals[j+dihe_count].atoms[3]=dihedrals[j].atoms[3]+atom_count;
+            }
         }
+        
         
         //These parameters are used to shift the index of the atoms/bonds/dihedrals.
         atom_count += membranes[i].get_num_of_nodes();
