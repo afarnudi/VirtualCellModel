22294192ae86a0bf830c74c23cbd81135226b504
ali <a.farnudi@physics.sharif.edu>
2021-06-17 12:11:36 +0200

    BugFix + Test: rewrote Bussi multi thermo drop newton 3

--- source/OpenMM/BussiGlobalThermostat.cpp
+++ source/OpenMM/BussiGlobalThermostat.cpp
@@ -93,83 +93,81 @@ void set_Bussi_Global_thermostat_multithermos_DropNewton3(MyOpenMMData* omm,
     omm->CustomIntegrator->addGlobalVariable("c_global", c );
     
     omm->CustomIntegrator->addGlobalVariable("dofs_globalCh", dofCh);
-    omm->CustomIntegrator->addGlobalVariable("dofs_globalMem", dofMem);
+    omm->CustomIntegrator->addGlobalVariable("dofs_globalMe", dofMem);
 
     omm->CustomIntegrator->addGlobalVariable("keCh", 0.0 );
-    omm->CustomIntegrator->addGlobalVariable("keMem", 0.0 );
+    omm->CustomIntegrator->addGlobalVariable("keMe", 0.0 );
     
     omm->CustomIntegrator->addGlobalVariable("alphaCh", 0.0 );
-    omm->CustomIntegrator->addGlobalVariable("alphaMem", 0.0 );
+    omm->CustomIntegrator->addGlobalVariable("alphaMe", 0.0 );
     
     omm->CustomIntegrator->addGlobalVariable("sign_alphaCh", 0.0 );
-    omm->CustomIntegrator->addGlobalVariable("sign_alphaMem", 0.0 );
+    omm->CustomIntegrator->addGlobalVariable("sign_alphaMe", 0.0 );
     
     omm->CustomIntegrator->addGlobalVariable("R_globalCh", 0.0 );
-    omm->CustomIntegrator->addGlobalVariable("R_globalMem", 0.0 );
+    omm->CustomIntegrator->addGlobalVariable("R_globalMe", 0.0 );
     
     omm->CustomIntegrator->addGlobalVariable("S_globalCh", 0.0 );
-    omm->CustomIntegrator->addGlobalVariable("S_globalMem", 0.0 );
+    omm->CustomIntegrator->addGlobalVariable("S_globalMe", 0.0 );
     
     omm->CustomIntegrator->addGlobalVariable("R2Ch", 0.0 );
-    omm->CustomIntegrator->addGlobalVariable("R2Mem", 0.0 );
+    omm->CustomIntegrator->addGlobalVariable("R2Me", 0.0 );
     
     omm->CustomIntegrator->addGlobalVariable("sum_R2Ch", 0.0 );
-    omm->CustomIntegrator->addGlobalVariable("sum_R2Mem", 0.0 );
+    omm->CustomIntegrator->addGlobalVariable("sum_R2Me", 0.0 );
     
     omm->CustomIntegrator->addGlobalVariable("global_b1Ch", ( 1-c )* 0.5 * kBT     );
-    omm->CustomIntegrator->addGlobalVariable("global_b1Mem", ( 1-c )* 0.5 * kBTmem     );
+    omm->CustomIntegrator->addGlobalVariable("global_b1Me", ( 1-c )* 0.5 * kBTmem     );
     
     omm->CustomIntegrator->addGlobalVariable("global_b2Ch", 2.0 * sqrt( c*(1-c)*0.5 * kBT ) );
-    omm->CustomIntegrator->addGlobalVariable("global_b2Mem", 2.0 * sqrt( c*(1-c)*0.5 * kBTmem ) );
+    omm->CustomIntegrator->addGlobalVariable("global_b2Me", 2.0 * sqrt( c*(1-c)*0.5 * kBTmem ) );
     
     omm->CustomIntegrator->addGlobalVariable("global_b3Ch", sqrt( c/( (1-c)* 0.5 * kBT) ) );
-    omm->CustomIntegrator->addGlobalVariable("global_b3Mem", sqrt( c/( (1-c)* 0.5 * kBTmem) ) );
-    
+    omm->CustomIntegrator->addGlobalVariable("global_b3Me", sqrt( c/( (1-c)* 0.5 * kBTmem) ) );
     omm->CustomIntegrator->addPerDofVariable("f_n31", 0.0);
-    
-    
-    omm->CustomIntegrator->addComputePerDof("f_n31", "f31");
     // global thermostat coefficients
-    omm->CustomIntegrator->addComputeSum("keCh", "0.5*m*v*v *delta(f_n31)");
-    omm->CustomIntegrator->addComputeSum("keMem","0.5*m*v*v *(1-delta(f_n31))");
+    
+    omm->CustomIntegrator->addUpdateContextState();
+    omm->CustomIntegrator->addComputePerDof("f_n31", "f31");
+    omm->CustomIntegrator->addComputeSum("keCh", "0.5*m*v*v*delta(f_n31)");
+    omm->CustomIntegrator->addComputeSum("keMe", "0.5*m*v*v*(1-delta(f_n31))");
     //"S_global" is an approximate since we assume "dofs_global" goes to inf
     //for "dofs_global" > 1000 (at least) this is a good approximation
     omm->CustomIntegrator->addComputeGlobal("R_globalCh", "gaussian");
-    omm->CustomIntegrator->addComputeGlobal("R_globalMem", "gaussian");
+    omm->CustomIntegrator->addComputeGlobal("R_globalMe", "gaussian");
     
     omm->CustomIntegrator->beginIfBlock("dofs_globalCh>1000");
-    omm->CustomIntegrator->addComputeGlobal("S_globalCh", "sqrt(2*(dofs_globalCh-1))*gaussian+(dofs_globalCh-1)*delta(f_n31)");
+    omm->CustomIntegrator->addComputeGlobal("S_globalCh", "sqrt(2*(dofs_globalCh-1))*gaussian+(dofs_globalCh-1)");
     omm->CustomIntegrator->endBlock();
-    omm->CustomIntegrator->beginIfBlock("dofs_globalMem>1000");
-    omm->CustomIntegrator->addComputeGlobal("S_globalMem", "sqrt(2*(dofs_globalMem-1))*gaussian+(dofs_globalMem-1)*(1-delta(f_n31))");
+    omm->CustomIntegrator->beginIfBlock("dofs_globalMe>1000");
+    omm->CustomIntegrator->addComputeGlobal("S_globalMe", "sqrt(2*(dofs_globalMe-1))*gaussian+(dofs_globalMe-1)");
     omm->CustomIntegrator->endBlock();
     
     omm->CustomIntegrator->beginIfBlock("dofs_globalCh<=1000");
     omm->CustomIntegrator->addComputePerDof("R2Ch", "gaussian^2");
-    omm->CustomIntegrator->addComputeSum("sum_R2Ch", "R2Ch*delta(f_n31)");
+    omm->CustomIntegrator->addComputeSum("sum_R2Ch", "R2Ch");
     omm->CustomIntegrator->addComputeGlobal("S_globalCh", "sum_R2Ch-gaussian^2");
     omm->CustomIntegrator->endBlock();
-    omm->CustomIntegrator->beginIfBlock("dofs_globalMem<=1000");
-    omm->CustomIntegrator->addComputePerDof("R2Mem", "gaussian^2");
-    omm->CustomIntegrator->addComputeSum("sum_R2Mem", "R2Mem*(1-delta(f_n31))");
-    omm->CustomIntegrator->addComputeGlobal("S_globalMem", "sum_R2Mem-gaussian^2");
+    omm->CustomIntegrator->beginIfBlock("dofs_globalMe<=1000");
+    omm->CustomIntegrator->addComputePerDof("R2Me", "gaussian^2");
+    omm->CustomIntegrator->addComputeSum("sum_R2Me", "R2Me");
+    omm->CustomIntegrator->addComputeGlobal("S_globalMe", "sum_R2Me-gaussian^2");
     omm->CustomIntegrator->endBlock();
     
     omm->CustomIntegrator->addComputeGlobal("alphaCh","sqrt(c_global + global_b1Ch*(S_globalCh+R_globalCh*R_globalCh)/keCh + global_b2Ch*R_globalCh*sqrt(1.0/keCh) )");
-    omm->CustomIntegrator->addComputeGlobal("alphaMem","sqrt(c_global + global_b1Mem*(S_globalMem+R_globalMem*R_globalMem)/keMem + global_b2Mem*R_globalMem*sqrt(1.0/keMem) )");
+    omm->CustomIntegrator->addComputeGlobal("alphaMe","sqrt(c_global + global_b1Me*(S_globalMe+R_globalMe*R_globalMe)/keMe + global_b2Me*R_globalMe*sqrt(1.0/keMe) )");
     
     omm->CustomIntegrator->addComputeGlobal("sign_alphaCh","2*step(R_globalCh + global_b3Ch*sqrt(keCh) )-1");
-    omm->CustomIntegrator->addComputeGlobal("sign_alphaMem","2*step(R_globalMem + global_b3Mem*sqrt(keMem) )-1");
+    omm->CustomIntegrator->addComputeGlobal("sign_alphaMe","2*step(R_globalMe + global_b3Me*sqrt(keMe) )-1");
     //momenta
-    omm->CustomIntegrator->addComputePerDof("v", "sign_alphaCh*alphaCh*v*delta(f_n31) + sign_alphaMem*alphaMem*v*(1-delta(f_n31))");
+    omm->CustomIntegrator->addComputePerDof("v", "(sign_alphaCh*alphaCh*delta(f_n31) + sign_alphaMe*alphaMe*(1-delta(f_n31)) )*v");
     //velocity-Verlet
     omm->CustomIntegrator->addUpdateContextState();
-    
     omm->CustomIntegrator->addComputePerDof("v", "v + 0.5*dt*(f0+f_n31)/m");
-    omm->CustomIntegrator->addComputePerDof("v", "v + 0.5*dt*f30*delta(f_n31)/m");
+    omm->CustomIntegrator->addComputePerDof("v", "v + 0.5*dt*( f30*delta(f_n31) )/m");
     
     omm->CustomIntegrator->addComputePerDof("x", "x+dt*v");
     
     omm->CustomIntegrator->addComputePerDof("v", "v + 0.5*dt*(f0+f_n31)/m");
-    omm->CustomIntegrator->addComputePerDof("v", "v + 0.5*dt*f30*delta(f_n31)/m");
+    omm->CustomIntegrator->addComputePerDof("v", "v + 0.5*dt*( f30*delta(f_n31) )/m");
 }
