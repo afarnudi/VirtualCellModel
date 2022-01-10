aca356ca507f742cf1fe9c7a2051d5d4046db20c
ali <ali.farnudi@ens-lyon.fr>
2022-01-10 16:52:41 +0100

    Removed the meanCurvature2 model. Finished testing. The model was wrong.

--- source/Membrane/Membrane_config.cpp
+++ source/Membrane/Membrane_config.cpp
@@ -223,8 +223,6 @@ void Membrane::assign_parameters(void){
                 bending_model = potentialModelIndex.Model["ExpDihedral"];
             } else if (split[0]=="meanCurvature") {
                 bending_model = potentialModelIndex.Model["meanCurvature"];
-            } else if (split[0]=="meanCurvature2") {
-                bending_model = potentialModelIndex.Model["meanCurvature"];
             } else {
                 string errorMessage = TWARN;
                 errorMessage+="Membrane config parser: Bending Model: I don't understand the \""+split[0]+"\" Model. Available models: cosine (cosine of dihedral angle), and N (None).";
