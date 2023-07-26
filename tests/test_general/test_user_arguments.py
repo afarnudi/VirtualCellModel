385391e6d6101849a293d209f11d41947057d8db
ali <ali.farnudi@ens-lyon.fr>
2023-07-26 12:52:52 +0200

    Add argparse tests with nose

---
+++ tests/test_general/test_user_arguments.py
@@ -0,0 +1,23 @@
+import pytest
+from source.general.argument_parser import create_parser
+from source.general.argument_parser import analyse_parser_argumetns
+
+
+def test_analyse_parser_template_generate_short_flag_register():
+    parser = create_parser()
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        user_args = parser.parse_args(["-g"])
+        user_inputs = analyse_parser_argumetns(user_args, parser)
+    # assert user_inputs.generate_template == True
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+
+
+def test_analyse_parser_template_generate_long_flag_register():
+    parser = create_parser()
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        user_args = parser.parse_args(["--generate-template"])
+        user_inputs = analyse_parser_argumetns(user_args, parser)
+    # assert user_inputs.generate_template == True
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
