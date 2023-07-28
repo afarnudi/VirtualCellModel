4de05761187495935cd7d1c7c00e8776ba70e32e
ali <ali.farnudi@ens-lyon.fr>
2023-07-28 13:57:51 +0200

    Add class parameters introduced in the argparse

--- src/general/classes/PlatformInfo.py
+++ src/general/classes/PlatformInfo.py
@@ -1,7 +1,11 @@
 class PlatformInfo:
-    pass
+    def __init__(self):
+        self.platform_name = "Reference"
+        self.platform_device_ID = 0
+        self.openmm_plugin_path = None
 
 
 def print_platform_info(user_inputs):
     print("print_platform_info is under development")
+    import sys
     sys.exit()
--- src/general/classes/UserInputs.py
+++ src/general/classes/UserInputs.py
@@ -4,3 +4,7 @@ from general.classes.PlatformInfo import PlatformInfo
 class UserInputs:
     def __init__(self):
         self.platform_info = PlatformInfo()
+        self.resume_path = None
+        self.config_file_path = None
+        self.config_file_path = None
+        self.write_at_end = False
