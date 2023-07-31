747af6b0d7bccaa346359c4c15a2e7b93ef842fc
ali <ali.farnudi@ens-lyon.fr>
2023-07-31 10:05:18 +0200

    seporate user input platform for parsing

--- src/general/classes/UserInputs.py
+++ src/general/classes/UserInputs.py
@@ -3,8 +3,8 @@ from general.classes.PlatformInfo import PlatformInfo
 
 class UserInputs:
     def __init__(self):
+        self.user_selected_platform = None
         self.platform_info = PlatformInfo()
         self.resume_path = None
-        self.config_file_path = None
         self.config_file_path = None
         self.write_at_end = False
